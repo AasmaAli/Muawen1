415bc30f4e7d5185c86460873ae0722348556ea7
ASMA R <asma.alradhyan@gmail.com>
2020-09-01 12:13:18 +0300

    the first one

---
+++ app/src/main/java/com/example/muawen_asma2/MainActivity.java
@@ -0,0 +1,195 @@
+package com.example.muawen_asma2;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DatabaseError;
+
+import org.jetbrains.annotations.NotNull;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private NavigationView navigationView;
+    private TextView NavProfileUserName;
+
+    private DrawerLayout drawerLayout;
+    private RecyclerView itemList;
+
+
+
+
+
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference UsersRef;
+    String currentUserID;
+
+    //private ImageButton But_Logout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawable_layout);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        View navView = navigationView.inflateHeaderView(R.layout.navigtion_header);
+        NavProfileUserName = (TextView) navView.findViewById(R.id.Username);
+
+        UsersRef.child(currentUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                if(dataSnapshot.exists())
+                {
+                    if(dataSnapshot.hasChild("username"))
+                    {
+                        String username = dataSnapshot.child("username").getValue().toString();
+                        NavProfileUserName.setText(username);
+                    }
+
+                    {
+                        Toast.makeText(MainActivity.this, "Profile name do not exists...", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item)
+            {
+                UserMenuSelector(item);
+                return false;
+            }
+        });
+
+    }//on Create this page
+
+    private void UserMenuSelector(MenuItem item) {
+        Intent Goto;
+        switch (item.getItemId())
+        {
+            case R.id.nav_Shoppinglist:
+                Goto = new Intent(MainActivity.this,ViewShoppingList.class);
+                startActivity(Goto);
+                break;
+
+            case R.id.nav_Orders:
+                Goto = new Intent(MainActivity.this,ViewOrders.class);
+                startActivity(Goto);
+                break;
+
+            case R.id.nav_Suggesteditem:
+                Goto = new Intent(MainActivity.this,Suggestionitem.class);
+                startActivity(Goto);
+                break;
+
+            case R.id.nav_AddItem:
+                Goto = new Intent(MainActivity.this,AddItem.class);
+                startActivity(Goto);
+                break;
+
+            case R.id.nav_DeleteItem:
+                Toast.makeText(this, "Delete Item", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.nav_Editprofile:
+                Goto = new Intent(MainActivity.this,EditProfile.class);
+                startActivity(Goto);
+                break;
+
+            case R.id.nav_loguot:
+                mAuth.signOut();
+                Intent loginIntent = new Intent(MainActivity.this,Register.class);
+
+                loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(loginIntent);
+                finish();
+                break;
+        }
+
+    }//UserMenuSelector
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser == null)
+        {
+            Intent loginIntent = new Intent(MainActivity.this, Register.class);
+            loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(loginIntent);
+            finish();
+
+        }//if end
+
+         else {
+
+            //Check User Existence
+            final String current_user_id = mAuth.getCurrentUser().getUid();
+
+            UsersRef.addValueEventListener(new ValueEventListener() {
+
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if (!dataSnapshot.hasChild(current_user_id)) {
+                        // Send User To Setup Activity
+                        Intent setupIntent = new Intent(MainActivity.this, setup.class);
+                        setupIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(setupIntent);
+                        //finish();
+                    }//end if
+
+                }
+                @Override
+                public void onCancelled( DatabaseError databaseError) {
+                    Intent setupIntent = new Intent(MainActivity.this, AddItem.class);
+                    setupIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(setupIntent);
+                }
+
+
+
+            });
+
+        }//else end
+
+    }
+}
